dc4f0da62afd3acc90315e9707fe5999f49d0269
omkargb <omkargb107@gmail.com>
2021-08-04 15:43:31 +0530

    [omkargb] Add : Finding Maximum of 3 strings with test cases implemented.

--- FindMaximumTest/MaxCheck.cs
+++ FindMaximumTest/MaxCheck.cs
@@ -8,27 +8,76 @@ namespace FindMaximumTest
         MaxNumCheck check = new MaxNumCheck();
 
         [Test]
-        //tc 2.1
-        public void GivenMaxNumberFirstPosition_WhenMaxTest_ShouldReturnTrue()
+        //tc 1.1
+        public void GivenMaxIntNumberFirstPosition_WhenMaxTest_ShouldReturnTrue()
         {
-            float result = check.FloatMax(20.20f, 10.10f, 30.30f);
-            Assert.AreEqual(30.30, result);
+            int result = check.IntMax(30, 20, 10);
+            Assert.AreEqual(30, result);
+        }
+
+        [Test]
+        //tc 1.2
+        public void GivenMaxIntNumberSecondPosition_WhenMaxTest_ShouldReturnTrue()
+        {
+            int result = check.IntMax(10, 30, 20);
+            Assert.AreEqual(30, result);
+        }
+
+        [Test]
+        //tc 1.3
+        public void GivenMaxIntNumberThirdPosition_WhenMaxTest_ShouldReturnTrue()
+        {
+            int result = check.IntMax(20, 10, 30);
+            Assert.AreEqual(30, result);
+        }
+
+        [Test]
+        //tc 2.1
+        public void GivenMaxFloatNumberFirstPosition_WhenMaxTest_ShouldReturnTrue()
+        {
+            float result = check.FloatMax(20.123f, 10.123f, 30.123f);
+            Assert.AreEqual(30.123f, result);
         }
 
         [Test]
         //tc 2.2
-        public void GivenMaxNumberSecondPosition_WhenMaxTest_ShouldReturnTrue()
+        public void GivenMaxFloatNumberSecondPosition_WhenMaxTest_ShouldReturnTrue()
         {
-            float result = check.FloatMax(20.20f, 30.30f, 10.10f);
-            Assert.AreEqual(30.30, result);
+            float result = check.FloatMax(20.123f, 30.123f, 10.123f);
+            Assert.AreEqual(30.123f, result);
         }
 
         [Test]
         //tc 2.3
-        public void GivenMaxNumberThirdPosition_WhenMaxTest_ShouldReturnTrue()
+        public void GivenMaxFloatNumberThirdPosition_WhenMaxTest_ShouldReturnTrue()
         {
-            float result = check.FloatMax(20.20f, 10.10f, 30.30f);
-            Assert.AreEqual(30.30, result);
+            float result = check.FloatMax(20.123f, 10.123f, 30.123f);
+            Assert.AreEqual(30.123f, result);
         }
+
+        [Test]
+        //tc 3.1
+        public void GivenMaxStringFirstPosition_WhenMaxTest_ShouldReturnTrue()
+        {
+            string result = check.StringMax("Pear", "Banana", "Apple");
+            Assert.AreEqual("Pear", result);
+        }
+
+        [Test]
+        //tc 3.2
+        public void GivenMaxStringSecondPosition_WhenMaxTest_ShouldReturnTrue()
+        {
+            string result = check.StringMax("Apple", "Pear", "Banana");
+            Assert.AreEqual("Pear", result);
+        }
+
+        [Test]
+        //tc 3.3
+        public void GivenMaxStringThirdPosition_WhenMaxTest_ShouldReturnTrue()
+        {
+            string result = check.StringMax("Apple", "Banana", "Pear");
+            Assert.AreEqual("Pear", result);
+        }
+
     }
 }
